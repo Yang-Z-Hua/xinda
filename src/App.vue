d08eb5d734e4a0c746e688d06fb5be5d2b408d14
CHT111 <cht1230@163.com>
2018-02-27 16:08:37 +0800

    图片

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,24 @@
 <template>
   <div id="app">
-    <h1>公共顶部</h1>
-    <router-link to="/outter/register">注册</router-link>
+
+
+    <div class="head">
+      <ul class="mmm">
+        <div class="left">
+          <li>欢迎来到信达！</li>
+          <li>登录</li>
+          <li>快速注册</li>
+        </div>
+        <div class="right">
+          <ul>购物车<span>0</span>件</ul>
+          <li>服务商入口</li>
+        </div>
+      </ul>
+    </div>
     <router-view></router-view>
-    <h1>公共底部</h1>
+    <div class="foot">ⒸCopyright 2016北京信达科技有限公司 京ICP备 16011621号</div>
+
+    
   </div>
 </template>
 
@@ -14,5 +29,44 @@ export default {
 </script>
 
 <style>
-*{padding: 0;margin: 0;} 
+  *{padding: 0;margin: 0;list-style: none} 
+  #app{
+    min-width: 1200px;
+  }
+  .head{
+    height: 35px;
+    background-color: #f2f2f2;
+    line-height: 35px;
+  }
+  .mmm{
+    width: 1200px;
+    margin:0 auto;
+    font-size: 12px;
+    display: flex
+  }
+  .mmm .left{
+    display: flex;
+    color: #2693d4;
+  }
+  .mmm .left li:nth-child(1){
+    color: black
+  }
+  .mmm .left li{
+    margin-right: 18px;
+  }
+  .mmm .right{
+    display:flex;
+    margin: 0 0 0 auto;
+  }
+  .mmm .right li{
+    color: #2693d4;
+    margin-left: 17px
+  }
+  .foot{
+    height: 41px;
+    background-color: #cbcccc;
+    text-align: center;
+    line-height: 41px;
+    font-size: 14px;
+  }
 </style>
--- src/components/Inner.vue
+++ src/components/Inner.vue
@@ -1,8 +1,17 @@
 <template>
   <div>
-    <h3>与首页类似的公共头部</h3>
+    <div class="head-1">
+
+    </div>
     <router-view></router-view>
-    <h3>与首页类似的公共脚部</h3>    
+    <div class="foot-1">
+      <div class="mmm-1">
+        <ul>关于我们</ul>
+        <li>联系我们：contact@xinkeher.com</li>
+        <li>公司地址：北京市朝阳区大望路soho现代城</li>
+        <li>官方客服电话:010-83421842</li>
+      </div>
+    </div>    
   </div>
 </template>
 
@@ -18,6 +27,28 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
+<style scoped lang='less'>
+.head-1{
+  width: 1200px;
+  height: ;
+}
+  .foot-1{
+    height: 200px;
+    background-color: #f8f8f8;
+  }
+  .foot-1 .mmm-1{
+    width: 1200px;
+    padding-left: 25px;
+    overflow: hidden;
+    margin: 0 auto;
+    line-height: 1;
+    li{
+      font-size: 12px;
+      margin-top: 12px;
+    }
+    ul{
+      font-size: 16px;
+      margin: 38px 0;
+    }
+  }
 </style>
