31d88df718a966b147ef12b57ad0f1492ab5f8ea
mxkk <469780687@qq.com>
2018-02-27 20:03:46 +0800

    会员

--- src/views/Huiyuanzhongxin.vue
+++ src/views/Huiyuanzhongxin.vue
@@ -1,6 +1,34 @@
 <template>
-  <div>
-    <h4>会员中心头部</h4>
+  <div class="public">
+    <div class="left">
+      <div class="top">
+        <p>首页</p>
+        <p>/</p>
+        <p>公司工商</p>
+      </div>
+      <div class="middle">
+        <div class="middle-top">
+          <div><img src="../assets/images/u5086.png" alt=""></div>
+          <p>12345678901</p>
+        </div>
+        <div class="bottom">
+          <div class="dingdan">
+            <span><img src="../assets/images/u5092.png" alt=""></span>
+            <p>我的订单</p>
+          </div>
+          <div>
+            <span><img src="../assets/images/u5098.png" alt=""></span>
+            <p>用户评价</p>
+          </div>
+          <div>
+            <span><img src="../assets/images/u5102.png" alt=""></span>
+            <p>账户设置</p>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+    
     <router-view></router-view>
   </div>
 </template>
@@ -17,6 +45,66 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped lang="less">
+.public{
+    width: 1200px;
+    margin: 26px auto;
+}
+.left{
+  display:inline-block;
+  width: 244px;
+  height:552px;
+}
+.top{
+  line-height: 14px;
+  font-size:14px;
+  p{
+    display: inline-block;
+  }
+}
 
+.middle{
+    width: 242px;
+    height:526px;
+    margin: 13px 0 0 2px;
+    .middle-top{
+      height:141px;
+      background-color: #e9e9e9;
+      div{
+          text-align: center;
+         img{
+          width: 100px;
+          height:100px;
+          margin-top: 9px;
+        }
+      }
+      p{
+        text-align: center;
+      }
+    }
+    .bottom{
+      height: 376px;
+      margin-top: 9px;
+      background-color: #f7f7f7;
+      div{
+        height: 50px;
+        text-align: center;
+        line-height: 50px;
+        p{
+          font-size: 17px;
+          line-height: 17px;
+          display: inline-block;
+        }
+        span{
+          img{
+            width: 22px;
+            height: 22px;
+          }
+        }
+      } 
+      .dingdan{
+        background-color:#e9e9e9;
+      }
+    }
+}
 </style>
