66a643545cff40c046ba54754db40f2a30923ff9
wuxu19950407 <1272737779@qq.com>
2018-03-08 15:03:00 +0800

    n

--- src/views/Dingdanxiangqing.vue
+++ src/views/Dingdanxiangqing.vue
@@ -4,46 +4,46 @@
     <p class="goods_one goods_two">订单详情</p>
     <div class="goods_thr">
       <ul>
-        <li>订单编号：<a class="goodsp_1">{{number_1}}</a></li>
-        <li>创建时间：2017-07-01 01：12：21</li>
-        <li>订单金额<a>{{sub}}</a>元</li>
+        <li>订单编号：<a class="goodsp_1">{{businessOrder.businessNo}}</a></li>
+        <li>创建时间：{{createTime}}</li>
+        <li>订单金额<a>{{businessOrder.totalPrice}}</a>元</li>
       </ul>
-      <ul>
-        <li>服务名称：注册分公司</li>
-        <li>单价：<a>{{sub_1}}</a>元</li>
-        <li>数量：<a>{{sub_a}}</a></li>
-        <li>服务总额：<a>{{sub_1*sub_a}}</a>元</li>
-      </ul>
-      <ul>
-        <li>服务名称：代理记账</li>
-        <li>单价：<a>{{sub_2}}</a>元</li>
-        <li>数量：<a>{{sub_b}}</a></li>
-        <li>服务总额：<a>{{sub_2*sub_b}}</a>元</li>
-      </ul>
+    <div class="goods_thr goods_thr2" v-for="(item,index) in serviceOrderList" :key="index">      
+        <ul>
+          <li>服务名称：{{item.serviceName}}</li>
+          <li>单价：<a>{{item.unitPrice}}</a>元</li>
+          <li>数量：<a>{{item.buyNum}}</a></li>
+          <li>服务总额：<a>{{item.unitPrice*item.buyNum}}</a>元</li>
+        </ul>
+    </div> 
     </div>
     <p class="goods_one goods_way">支付方式</p>
     <p class="goods_one goods_noway">非网银支付</p>
     <div class="goods_box1">
-      <input type="radio" class="goods_d">
+      <input type="radio" class="goods_d" name="fname" @click="sbank">
+      <img src="../assets/images/dingdan1.jpg" alt="" class="iimg1">
     </div>
     <p class="goods_one goods_noway">平台支付</p>
     <div class="goods_bb">    
       <div class="goods_box1">
-        <input type="radio" class="goods_d">
+        <input type="radio" class="goods_d" name="fname">
+        <img src="../assets/images/dingdan2.jpg" alt="" class="iimg2">        
       </div>
       <div class="goods_box1">
-        <input type="radio" class="goods_d">
+        <input type="radio" class="goods_d" name="fname">
+        <img src="../assets/images/dingdan3.jpg" alt="" class="iimg2">        
       </div>
     </div>
     <a class="goods_one goods_zizhu">自助转账</a><a class="goods_xiaozi">因限额不能支付时，建议使用自助转账</a>
     <div class="goods_bank">
-        <input type="radio" class="goods_d goods_ds">      
+      <input type="radio" class="goods_d goods_ds" name="fname">
+      <img src="../assets/images/dingdan.jpg" alt="" class="iimg3">              
     </div>
     <p class="goods_red">注：转账时请将订单编号备注在付款信息里：转账完成后，请通知客服</p>
     <div class="goods_jiesuan">
-      <a class="goods_am">金额总计</a><a class="goods_shuzi">￥{{all}}</a>
-      <div class="goods_kuang"><a href="#" class="goods_end">去结算</a></div>
-    </div>    
+      <a class="goods_am">金额总计</a><a class="goods_shuzi">￥{{all}}.00</a>
+      <div class="goods_kuang"><a :href="asd" class="goods_end">去结算</a></div>
+    </div> 
   </div>
 </template>
 
@@ -53,27 +53,52 @@ export default {
   data () {
     return {
       msg: 'Welcome to Your Vue.js App',
-      number_1:'S6272827272727211',
-      sub:'2000.00',
       sub_1:'800.00',
       sub_a:'1',
       sub_2:'1800.00',
       sub_b:'1',
       iiiiii:'',
-      all: '800.00'
+      all: '',
+      asd:'#',
+      businessOrder:'',
+      serviceOrderList:'',
+      sd:'',
+      createTime:'',
     }
   },
   methods:{
-    
-  },
-    created() {
-    this.ajax.post('/xinda-api/pay/detail',this.qs.stringify({
-
+  timestampToTime(we) {
+        var date = new Date(we);
+        var Y = date.getFullYear() + '-';
+        var M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
+        var D = date.getDate() + ' ';
+        var h = date.getHours() + ':';
+        var m = date.getMinutes() + ':';
+        var s = date.getSeconds();
+        return Y+M+D+h+m+s;
+    },
+  sbank(){
+    this.ajax.post('/xinda-api/pay/china-pay',this.qs.stringify({
+      // businessNo:S1704040001075133085
     }))
     .then((data)=>{
-      console.log(data)
+      console.log(data.data.data)
     })
-  }
+    }
+  },
+  created() {
+    this.all = this.$route.query.op
+    this.ajax.post('/xinda-api/business-order/detail',this.qs.stringify({
+      businessNo:this.$route.query.businessNo
+    }))
+    .then((data)=>{
+      console.log(data.data.data)
+      this.businessOrder = data.data.data.businessOrder;
+      this.serviceOrderList = data.data.data.serviceOrderList;
+      this.sd = data.data.data.businessOrder.createTime
+      this.createTime = this.timestampToTime(this.sd)
+    })
+  },
 
 }
 </script>
@@ -81,8 +106,7 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
 .goods_all{
-  width: 1238px;
-  height: 1008px;
+  width: 1238px;   
   margin: 7px auto 0;
   font-size: 14px;
 }
@@ -97,21 +121,21 @@ export default {
 }
 // 订单详细信息
 .goods_thr{
-  width: 1202px;
-  height: 227px;
-  margin: 38px auto 0;
-  border: 1px solid #b6b6b6;
+  margin-top: 38px;
   ul{
     height: 75px;
-    border-bottom: 1px solid #b6b6b6;
     display: flex;
+    background-color: #f7f7f7;
+    border: 1px solid #b6b6b6;
     li{
       width: 400px;
       text-align: center;
       line-height: 70px;
-      background-color: #f7f7f7;
     }
   }
+}
+.goods_thr2{
+  margin-top: 0;
 }
 a{
   color: #62a8dc;
@@ -131,12 +155,22 @@ a{
   height: 50px;
   border: 1px solid #efefef;
   margin-left: 24px;
+  .iimg1{
+    margin: 5px 0 0 30px;
+  }
+  .iimg2{
+    margin: 9px 0 0 30px;    
+  }
 }
+.iimg3{
+    margin: 5px 0 0 30px;    
+  }
 .goods_bb{
   display: flex;
 }
 .goods_d{
-  margin: 17px 0 0 3px;
+  margin: -24px 0 0 3px;
+  vertical-align: middle;
 }
 // bank
 .goods_zizhu{
@@ -153,9 +187,6 @@ a{
   height: 68px;
   border: 1px solid #efefef;
   margin: 23px 0 0 24px;
-}
-.goods_ds{
-  margin-top: 28px;
 }
 // 红字
 .goods_red{
